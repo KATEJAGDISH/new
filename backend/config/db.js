e09cdea3f75088b98e3659f316580d5235b35a6c
KATEJAGDISH <jagdishkate8080@gmail.com>
2025-06-01 13:23:18 +0530

    frist-commiti

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://jagdishkate8080:8424013789@cluster0.oahbtul.mongodb.net/cars', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
